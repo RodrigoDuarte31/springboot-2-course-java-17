3395036318aa829cd2b599dd8b8ce80e06f605da
rodrigo <rodrigoduarte31@gmail.com>
2022-04-18 10:46:56 -0300

    Category

---
+++ src/main/java/com/rodrigo/projetospring/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigo.projetospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rodrigo.projetospring.entities.Category;
+
+public interface CategoryRepository  extends JpaRepository<Category, Long> {
+	
+	
+
+}
