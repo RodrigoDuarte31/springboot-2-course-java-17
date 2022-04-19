8349bd6f87648c9dbfd176cf5815ea7e7aaafb2e
rodrigo <rodrigoduarte31@gmail.com>
2022-04-19 15:39:39 -0300

    ExceptionHandling - findById

--- src/main/java/com/rodrigo/projetospring/services/UserService.java
+++ src/main/java/com/rodrigo/projetospring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.rodrigo.projetospring.entities.User;
 import com.rodrigo.projetospring.repositories.UserRepository;
+import com.rodrigo.projetospring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
